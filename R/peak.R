19daf44605f58b91f19268c1676c58b73a0d3c88
Brandon Monier <brandon.monier@gmail.com>
2020-01-06 17:54:59 -0500

    Add documentation

--- R/peak.R
+++ R/peak.R
@@ -3,6 +3,8 @@
 #' @description Reveals first i rows and j columns.
 #'
 #' @param x An R data frame or matrix object
+#' @param pi How many rows do you want to select? Defaults to \code{10}.
+#' @param pj How many columns do you want to select? Defaults to \code{4}.
 #'
 #' @export
 peak <- function(x, pi = 10, pj = 4) {
